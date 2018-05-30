d82264735af82e02ed7d945a4c2f5fe0ec16e8be
adrixop95 <adrixop95@me.com>
2018-05-30 21:34:36 +0200

    init

---
+++ source/src/przelew.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author adria
+ */
+public class przelew {
+            
+    private String kontoBankowe = "";
+    private String tytulPrzelewu = "";
+    
+    public static void przelew(){
+
+    }
+    
+}
